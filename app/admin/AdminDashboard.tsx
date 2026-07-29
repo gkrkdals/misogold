743071c9c9acc7158a38b3379a46876fe0f32a91
gkrkdals <hagangmin@gmail.com>
2026-07-30 00:32:21 +0900

    feat: misogold 세부 정보 입력

--- app/admin/AdminDashboard.tsx
+++ app/admin/AdminDashboard.tsx
@@ -347,7 +347,7 @@ export default function AdminDashboard({ initialPrices, initialImages }: AdminDa
             </svg>
             오늘의 금시세 관리
           </button>
-          <button
+          {/* <button
             onClick={() => setActiveTab("images")}
             className={`pb-4 px-4 font-bold text-sm tracking-wide transition-all border-b-2 flex items-center gap-2 cursor-pointer ${
               activeTab === "images"
@@ -359,7 +359,7 @@ export default function AdminDashboard({ initialPrices, initialImages }: AdminDa
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
             </svg>
             홍보 이미지 영역 관리
-          </button>
+          </button> */}
         </div>
 
         {/* Tab Contents: Prices */}
